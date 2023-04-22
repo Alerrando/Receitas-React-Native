bbb61e8f6bd0d8fc71b3b857dc4cb3401957e1bb
Alerrando Breno <alerrando2@gmail.com>
2023-04-22 14:03:49 -0300

    add gradiente to foodlist

--- src/components/FoodList/index.js
+++ src/components/FoodList/index.js
@@ -1,11 +1,28 @@
-import { Image, StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { LinearGradient } from 'expo-linear-gradient'
 
 export function FoodList({ item }){
 
     return(
-        <View style={styles.container}>
+        <TouchableOpacity activeOpacity={0.9} style={styles.container}>
             <Image source={{ uri: item.cover }} style={styles.containerImg} />
-        </View>
+
+            <View style={styles.containerInfos}>
+                <Text style={styles.containerInfosTextH1}>{item.name}</Text>
+                
+                <View style={styles.containerInfosIngredients}>
+                    <Text style={styles.containerInfosText}>{`${item.ingredients.length + 1} ingredientes`}</Text>
+                    <Text style={styles.containerInfosText}>|</Text>
+                    <Text style={styles.containerInfosText}>{`${item.time} min`}</Text>
+                </View>
+            </View>
+
+            <LinearGradient 
+                style={styles.gradient} 
+                colors={['transparent', 'rgba(0, 0, 0, 0.70)', 'rgba(0, 0, 0, 0.95)']} 
+            />
+
+        </TouchableOpacity>
     )
 }
 
@@ -14,12 +31,50 @@ const styles = StyleSheet.create({
         color: "#fff",
         height: 200,
         marginBottom: 14,
+        position: "relative",
         width: '100%',
     },
 
     containerImg: {
+        backgroundColor: "linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, #000000 100%)",
         borderRadius: 14,
         height: "100%",
         width: "100%",
-    }
+    },
+
+    containerInfos: {
+        bottom: 0,
+        padding: 31,
+        position: 'absolute',
+        zIndex: 100,
+    },
+
+    containerInfosIngredients: {
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        paddingTop: 9,
+        width: 161,
+    },
+
+    containerInfosText: {
+        color: "#fff",
+        fontSize: 14,
+        fontWeight: "400",
+    },
+
+    containerInfosTextH1: {
+        color: '#fff',
+        fontSize: 18,
+        fontWeight: "bold",
+    },
+
+    gradient: {
+        borderRadius: 14,
+        bottom: 0,
+        height: "55%",
+        left: 0,
+        right: 0,
+        position: "absolute",
+    },
 })
